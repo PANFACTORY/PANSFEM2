e632cdd07ebff465d309748a31b107718e780a80
PANFACTORY <yutatanabe510@gmail.com>
2019-11-10 19:52:53 +0900

    Modify PlaneStrain

--- src/FEM/Equation/PlaneStrain.h
+++ src/FEM/Equation/PlaneStrain.h
@@ -1,5 +1,5 @@
 //*****************************************************************************
-//Title		:PANSFEM2/FEM/Equation/PlaneStrain.h
+//Title		:src/FEM/Equation/PlaneStrain.h
 //Author	:Tanabe Yuta
 //Date		:2019/10/02
 //Copyright	:(C)2019 TanabeYuta
@@ -11,31 +11,52 @@
 #include <cassert>
 
 
-#include "../../LinearAlgebra/Models/LAOperation.h"
+#include "../../LinearAlgebra/Models/Matrix.h"
+#include "../../LinearAlgebra/Models/Vector.h"
 
 
 namespace PANSFEM2 {
-	//******************************平面ひずみモデルの剛性マトリクスを生成******************************
-	template<class T>
-	std::vector<std::vector<T> > PlaneStrainTri(std::vector<std::vector<T> >& _nodes, std::vector<int>& _element, T _E, T _V, T _t) {
-		//.....要素面積.....
-		T A = 0.5*((_nodes[_element[0]][0] - _nodes[_element[2]][0])*(_nodes[_element[1]][1] - _nodes[_element[2]][1]) - (_nodes[_element[2]][1] - _nodes[_element[0]][1])*(_nodes[_element[2]][0] - _nodes[_element[1]][0]));
+	//******************************Make element stiffness matrix******************************
+	template<class T, template<class>class SF, template<class>class IC>
+	void PlaneStrain(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<int>& _element, T _E, T _V, T _t) {
+		//----------Initialize element stiffness matrix----------
+		_Ke = Matrix<T>(2 * _element.size(), 2 * _element.size());
 
-		//.....Bマトリクス.....
-		std::vector<std::vector<T> > B = std::vector<std::vector<T> >(3, std::vector<T>(6));
-		B[0][0] = _nodes[_element[1]][1] - _nodes[_element[2]][1];	B[0][1] = 0.0;												B[0][2] = _nodes[_element[2]][1] - _nodes[_element[0]][1];	B[0][3] = 0.0;												B[0][4] = _nodes[_element[0]][1] - _nodes[_element[1]][1];	B[0][5] = 0.0;
-		B[1][0] = 0.0;												B[1][1] = _nodes[_element[2]][0] - _nodes[_element[1]][0];	B[1][2] = 0.0;												B[1][3] = _nodes[_element[0]][0] - _nodes[_element[2]][0];	B[1][4] = 0.0;												B[1][5] = _nodes[_element[1]][0] - _nodes[_element[0]][0];
-		B[2][0] = _nodes[_element[2]][0] - _nodes[_element[1]][0];	B[2][1] = _nodes[_element[1]][1] - _nodes[_element[2]][1];	B[2][2] = _nodes[_element[0]][0] - _nodes[_element[2]][0];	B[2][3] = _nodes[_element[2]][1] - _nodes[_element[0]][1];	B[2][4] = _nodes[_element[1]][0] - _nodes[_element[0]][0];	B[2][5] = _nodes[_element[0]][1] - _nodes[_element[1]][1];
-		B /= (2.0*A);
+		//----------Generate cordinate matrix X----------
+		Matrix<T> X = Matrix<T>(_element.size(), 2);
+		for(int i = 0; i < _element.size(); i++){
+			for(int j = 0; j < 2; j++){
+				X(i, j) = _x[_element[i]](j);
+			}
+		}
 
-		//.....Dマトリクス.....
-		std::vector<std::vector<T> > D = std::vector<std::vector<T> >(3, std::vector<T>(3));
-		D[0][0] = 1.0 - _V;	D[0][1] = _V;		D[0][2] = 0.0;
-		D[1][0] = D[0][1];	D[1][1] = 1.0 - _V;	D[1][2] = 0.0;
-		D[2][0] = D[0][2];	D[2][1] = D[1][2];	D[2][2] = 0.5*(1.0 - 2.0*_V);
+		//----------Generate D matrix----------
+		Matrix<T> D = Matrix<T>(3, 3);
+		D(0, 0) = 1.0 - _V;	D(0, 1) = _V;		D(0, 2) = 0.0;
+		D(1, 0) = D(0, 1);	D(1, 1) = 1.0 - _V;	D(1, 2) = 0.0;
+		D(2, 0) = D(0, 2);	D(2, 1) = D(1, 2);	D(2, 2) = 0.5*(1.0 - 2.0*_V);
 		D *= (_E / ((1.0 - 2.0*_V)*(1.0 + _V)));
 
-		//.....Keマトリクスを計算して返す.....
-		return Transpose(B)*D*B*A*_t;
+		//----------Loop of Gauss Integration----------
+		for (int g = 0; g < IC<T>::N; g++) {
+			//----------Get difference of shape function----------
+			Matrix<T> dNdr = SF<T>::dNdr(IC<T>::Points[g]);
+
+			//----------Get difference of shape function----------
+			Matrix<T> dXdr = dNdr * X;
+			T J = dXdr.Determinant();
+			Matrix<T> dNdX = dXdr.Inverse() * dNdr;
+
+			//----------Generate B matrix----------
+			Matrix<T> B = Matrix<T>(3, 2 * _element.size());
+			for (int n = 0; n < _element.size(); n++) {
+				B(0, 2 * n) = dNdX(0, n);	B(0, 2 * n + 1) = 0.0;			
+				B(1, 2 * n) = 0.0;			B(1, 2 * n + 1) = dNdX(1, n);	
+				B(2, 2 * n) = dNdX(1, n);	B(2, 2 * n + 1) = dNdX(0, n);	
+			}
+
+			//----------Update element stiffness matrix----------
+			_Ke += B.Transpose()*D*B*J*_t*IC<T>::Weights[g][0] * IC<T>::Weights[g][1];
+		}
 	}
 }

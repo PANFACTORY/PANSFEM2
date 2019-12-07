a77be5fc883623d3259ca7cdd79fbb288a53d253
PANFACTORY <yutatanabe510@gmail.com>
2019-12-07 15:53:13 +0900

    Add sample of Newmark beta method

---
+++ sample/optimize_dynamic/sample_newmarkbeta.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+#include "../../src/LinearAlgebra/Models/Vector.h"
+#include "../../src/LinearAlgebra/Models/Matrix.h"
+#include "../../src/LinearAlgebra/Models/LILCSR.h"
+#include "../../src/PrePost/Import/ImportFromCSV.h"
+#include "../../src/FEM/Controller/Assembling.h"
+#include "../../src/FEM/Equation/PlaneStrain.h"
+#include "../../src/FEM/Controller/BoundaryCondition.h"
+#include "../../src/LinearAlgebra/Solvers/CG.h"
+#include "../../src/PrePost/Export/ExportToVTK.h"
+#include "../../src/FEM/Controller/ShapeFunction.h"
+#include "../../src/FEM/Controller/IntegrationConstant.h"
+
+
+using namespace PANSFEM2;
+
+
+int main() {
+	//----------Model Path----------
+	std::string model_path = "sample/optimize_dynamic/";
+	
+	//----------Add Nodes----------
+	std::vector<Vector<double> > nodes;
+	ImportNodesFromCSV(nodes, model_path + "Node.csv");
+	
+	//----------Add Elements----------
+	std::vector<std::vector<int> > elements;
+	ImportElementsFromCSV(elements, model_path + "Element.csv");
+	
+	//----------Add Field----------
+	std::vector<int> field;
+	int KDEGREE = 0;
+	ImportFieldFromCSV(field, KDEGREE, nodes.size(), model_path + "Field.csv");
+
+	//----------Add Dirichlet Condition----------
+	std::vector<int> isufixed;
+	std::vector<double> ufixed;
+	ImportDirichletFromCSV(isufixed, ufixed, field, model_path + "Dirichlet.csv");
+
+	//----------Add Neumann Condition----------
+	std::vector<int> isqfixed;
+	std::vector<double> qfixed;
+	ImportNeumannFromCSV(isqfixed, qfixed, field, model_path + "Neumann.csv");
+    	
+	//----------Define design parameters----------
+	double E = 210000.0;
+	double Poisson = 0.3;
+	
+	//----------Time step loop----------
+	for(int t = 0; t < 100; t++){
+		std::cout << "t = " << t << "\t";
+
+
+        //*************************************************
+        //  Direct analysis with Newmark beta method
+        //*************************************************
+    
+        //----------Assembling----------
+		LILCSR<double> K = LILCSR<double>(KDEGREE, KDEGREE);
+        std::vector<double> F = std::vector<double>(KDEGREE, 0.0);
+		for (int i = 0; i < elements.size(); i++) {
+			Matrix<double> Ke;
+			PlaneStrain<double, ShapeFunction8Square, Gauss9Square >(Ke, nodes, elements[i], E, Poisson, 1.0);
+			Assembling(K, Ke, elements[i], field);
+		}
+
+        //----------Set Neumann Boundary Condition----------
+	    SetNeumann(F, isqfixed, qfixed);
+
+		//----------Set Dirichlet Boundary Condition----------
+		SetDirichlet(K, F, isufixed, ufixed, 1.0e10);
+        
+        //----------Solve linear system----------
+        CSR<double> Kmod = CSR<double>(K);	
+        std::vector<double> d = ScalingCG(Kmod, F, 100000, 1.0e-10);
+                
+
+        //*************************************************
+        //  Post Process
+        //*************************************************
+        std::vector<Vector<double> > dv;
+		FieldResultToNodeValue(d, dv, field);
+		std::ofstream fout(model_path + "result" + std::to_string(t) + ".vtk");
+		MakeHeadderToVTK(fout);
+		AddPointsToVTK(nodes, fout);
+		AddElementToVTK(elements, fout);
+		AddElementTypes(std::vector<int>(elements.size(), 23), fout);
+		AddPointVectors(dv, "d", fout, true);
+		fout.close();
+	}
+	
+	return 0;
+}

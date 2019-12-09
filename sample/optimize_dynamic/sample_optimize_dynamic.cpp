f1cbf33166727fb5b446b59f9ea765a613ca05da
PANFACTORY <yutatanabe510@gmail.com>
2019-12-09 09:03:49 +0900

    Modify sample_optimize_dynamic.cpp

--- sample/optimize_dynamic/sample_optimize_dynamic.cpp
+++ sample/optimize_dynamic/sample_optimize_dynamic.cpp
@@ -46,84 +46,251 @@ int main() {
 	std::vector<double> qfixed;
 	ImportNeumannFromCSV(isqfixed, qfixed, field, model_path + "Neumann.csv");
 
+	//----------Initialize design variables----------
+	std::vector<double> s = std::vector<double>(elements.size(), 0.5);
+
 	//----------Define parameters----------
-	double E = 210000.0;				//	Young moduls
-	double Poisson = 0.3;				//	Poisson ratio
-	double rho = 0.0000078;				//	Density
+	double E0 = 0.001;
+	double E1 = 210000.0;				
+	double Poisson = 0.3;
+	double rho0 = 0.0;
+	double rho1 = 1.0;			
+	double Density = 0.0000078;	
+	double p = 3.0;			
 
-	double dt = 0.001;					//	Time step
-	int step = 101;						//	Step number
+	double dt = 0.001;					
+	int step = 101;						
 
-	double beta = 0.3333333333;			//	Parameter beta for Newmark beta method
-	double ganma = 0.5;					//	Parameter ganma for Newmark beta method
+	double beta = 0.3333333333;			
+	double ganma = 0.5;					
 
-	//----------Add Initial Condition----------
-	std::vector<std::vector<Vector<double> > > d = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Displacement of nodes
-	std::vector<std::vector<Vector<double> > > v = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Velocity of nodes
-	std::vector<std::vector<Vector<double> > > a = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Acceraration of nodes
-    		
-	//----------Time step loop----------
-	for(int t = 1; t < step; t++){
-		std::cout << "t = " << (double)t*dt << std::endl;
+	double iota = 0.75;
+	double lambdamin = 1.0e-20;
+	double lambdamax = 1.0e20;
+	double lambdaeps = 1.0e-15;
+	double movelimit = 0.15;
+
+	double weightlimit = 0.3;
+	double objectivebefore = 0.0;
+	double objectiveeps = 1.0e-5;
+
+	//----------Optimize loop----------
+	for(int k = 0; k < 100; k++){
+		std::cout << "k = " << k << "\t";
 
 
-        //*************************************************
-        //  Direct analysis with Newmark beta method
-        //*************************************************
-    
-        //----------Assembling----------
-		LILCSR<double> A = LILCSR<double>(KDEGREE, KDEGREE);
-        std::vector<double> b = std::vector<double>(KDEGREE, 0.0);
-		for (int i = 0; i < elements.size(); i++) {
-			Vector<double> dne = Vector<double>();
-			Vector<double> vne = Vector<double>();
-            Vector<double> ane = Vector<double>();
-            for(int j = 0; j < elements[i].size(); j++){
-                dne = dne.Vstack(d[t - 1][elements[i][j]]);
-                vne = vne.Vstack(v[t - 1][elements[i][j]]);
-                ane = ane.Vstack(a[t - 1][elements[i][j]]);
-            }
-
-			Matrix<double> Ke, Me;
-			PlaneStrain<double, ShapeFunction8Square, Gauss9Square >(Ke, nodes, elements[i], E, Poisson, 1.0);
-			PlaneMass<double, ShapeFunction8Square, Gauss9Square >(Me, nodes, elements[i], rho, 1.0);
-
-			Matrix<double> Ae = Me + pow(dt, 2.0)*beta*Ke;
-			Vector<double> be = -Ke*(dne + dt*vne + pow(dt, 2.0)*(0.5 - beta)*ane);
-
-			Assembling(A, b, Ae, be, elements[i], field);
+		//*************************************************
+		//  Get weight value and sensitivities
+		//*************************************************
+        double weight = 0.0;													//Function values of weight
+		std::vector<double> dweights = std::vector<double>(s.size());			//Sensitivities of weight
+        for (int i = 0; i < elements.size(); i++) {						
+			weight += rho0*(1.0 - s[i]) + rho1*s[i] - weightlimit;
+			dweights[i] = - rho0 + rho1;
 		}
 
-        //----------Set Neumann Boundary Condition----------
-		if(t == 1){
-			SetNeumann(b, isqfixed, qfixed);
-		}
+
+		//*************************************************
+		//  Direct time step with Newmark beta method
+		//*************************************************
+		std::vector<std::vector<Vector<double> > > d = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Displacement of nodes
+		std::vector<std::vector<Vector<double> > > v = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Velocity of nodes
+		std::vector<std::vector<Vector<double> > > a = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Acceraration of nodes
+
+		for(int t = 1; t < step; t++){
+			//----------Assembling----------
+			LILCSR<double> A = LILCSR<double>(KDEGREE, KDEGREE);
+			std::vector<double> b = std::vector<double>(KDEGREE, 0.0);
+			for (int i = 0; i < elements.size(); i++) {
+				Vector<double> dne = Vector<double>();
+				Vector<double> vne = Vector<double>();
+				Vector<double> ane = Vector<double>();
+				for(int j = 0; j < elements[i].size(); j++){
+					dne = dne.Vstack(d[t - 1][elements[i][j]]);
+					vne = vne.Vstack(v[t - 1][elements[i][j]]);
+					ane = ane.Vstack(a[t - 1][elements[i][j]]);
+				}
+
+				Matrix<double> Ke, Me;
+				double E = E0*(1.0 - pow(s[i], p)) + E1*pow(s[i], p);
+				double D = Density*(rho0*(1.0 - s[i]) + rho1*s[i]);
+				PlaneStrain<double, ShapeFunction8Square, Gauss9Square >(Ke, nodes, elements[i], E, Poisson, 1.0);
+				PlaneMass<double, ShapeFunction8Square, Gauss9Square >(Me, nodes, elements[i], D, 1.0);
+
+				Matrix<double> Ae = Me + pow(dt, 2.0)*beta*Ke;
+				Vector<double> be = -Ke*(dne + dt*vne + pow(dt, 2.0)*(0.5 - beta)*ane);
+
+				Assembling(A, b, Ae, be, elements[i], field);
+			}
+
+			//----------Set Neumann Boundary Condition----------
+			if(t == 1){
+				SetNeumann(b, isqfixed, qfixed);
+			}
+			
+			//----------Set Dirichlet Boundary Condition----------
+			SetDirichlet(A, b, isufixed, ufixed, 1.0e10);
 		
-		//----------Set Dirichlet Boundary Condition----------
-		SetDirichlet(A, b, isufixed, ufixed, 1.0e10);
-     
-        //----------Solve linear system----------
-        CSR<double> Amod = CSR<double>(A);	
-        std::vector<double> results = ScalingCG(Amod, b, 100000, 1.0e-10);
+			//----------Solve linear system----------
+			CSR<double> Amod = CSR<double>(A);	
+			std::vector<double> results = ScalingCG(Amod, b, 100000, 1.0e-10);
 
-		//----------Get d, v, a at step n+1----------
-		FieldResultToNodeValue(results, a[t], field);
-		for(int i = 0; i < nodes.size(); i++){
-			d[t][i] = d[t - 1][i] + dt*v[t - 1][i] + pow(dt, 2.0)*(0.5 - beta)*a[t - 1][i] + pow(dt, 2.0)*beta*a[t][i];
-			v[t][i] = v[t - 1][i] + dt*(1.0 - ganma)*a[t - 1][i] + dt*ganma*a[t][i];
+			//----------Get d, v, a at step n+1----------
+			FieldResultToNodeValue(results, a[t], field);
+			for(int i = 0; i < nodes.size(); i++){
+				d[t][i] = d[t - 1][i] + dt*v[t - 1][i] + pow(dt, 2.0)*(0.5 - beta)*a[t - 1][i] + pow(dt, 2.0)*beta*a[t][i];
+				v[t][i] = v[t - 1][i] + dt*(1.0 - ganma)*a[t - 1][i] + dt*ganma*a[t][i];
+			}			
 		}
 
-        //*************************************************
-        //  Post Process
-        //*************************************************
-		std::ofstream fout(model_path + "result" + std::to_string(t) + ".vtk");
+
+		//*************************************************
+		//  Invert time step with Newmark beta method
+		//*************************************************
+		std::vector<std::vector<Vector<double> > > l = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Displacement of nodes
+		std::vector<std::vector<Vector<double> > > m = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Velocity of nodes
+		std::vector<std::vector<Vector<double> > > n = std::vector<std::vector<Vector<double> > >(step, std::vector<Vector<double> >(nodes.size(), Vector<double>(2)));	//	Acceraration of nodes
+
+		for(int t = 1; t < step; t++){
+			//----------Assembling----------
+			LILCSR<double> A = LILCSR<double>(KDEGREE, KDEGREE);
+			std::vector<double> b = std::vector<double>(KDEGREE, 0.0);
+			for (int i = 0; i < elements.size(); i++) {
+				Vector<double> lne = Vector<double>();
+				Vector<double> mne = Vector<double>();
+				Vector<double> nne = Vector<double>();
+				Vector<double> dne = Vector<double>();
+ 				for(int j = 0; j < elements[i].size(); j++){
+					lne = lne.Vstack(l[t - 1][elements[i][j]]);
+					mne = mne.Vstack(m[t - 1][elements[i][j]]);
+					nne = nne.Vstack(n[t - 1][elements[i][j]]);
+					dne = dne.Vstack(d[step - t][elements[i][j]]);
+				}
+
+				Matrix<double> Ke, Me;
+				double E = E0*(1.0 - pow(s[i], p)) + E1*pow(s[i], p);
+				double D = Density*(rho0*(1.0 - s[i]) + rho1*s[i]);
+				PlaneStrain<double, ShapeFunction8Square, Gauss9Square >(Ke, nodes, elements[i], E, Poisson, 1.0);
+				PlaneMass<double, ShapeFunction8Square, Gauss9Square >(Me, nodes, elements[i], D, 1.0);
+
+				Matrix<double> Ae = Me + pow(dt, 2.0)*beta*Ke;
+				Vector<double> be = - Ke*dne - Ke*(lne + dt*mne + pow(dt, 2.0)*(0.5 - beta)*nne);
+
+				Assembling(A, b, Ae, be, elements[i], field);
+			}
+
+			//----------Set Dirichlet Boundary Condition----------
+			SetDirichlet(A, b, isufixed, ufixed, 1.0e10);
+		
+			//----------Solve linear system----------
+			CSR<double> Amod = CSR<double>(A);	
+			std::vector<double> results = ScalingCG(Amod, b, 100000, 1.0e-10);
+
+			//----------Get d, v, a at step n+1----------
+			FieldResultToNodeValue(results, n[t], field);
+			for(int i = 0; i < nodes.size(); i++){
+				l[t][i] = l[t - 1][i] + dt*m[t - 1][i] + pow(dt, 2.0)*(0.5 - beta)*n[t - 1][i] + pow(dt, 2.0)*beta*n[t][i];
+				m[t][i] = m[t - 1][i] + dt*(1.0 - ganma)*n[t - 1][i] + dt*ganma*n[t][i];
+			}			
+		}
+
+
+		//*************************************************
+		//	Integration by time
+		//*************************************************
+		double objective = 0.0;													//Function value of compliance
+		std::vector<double> dobjectives = std::vector<double>(s.size(), 0.0);	//Sensitivities of compliance
+		for(int t = 1; t < step; t++){
+			for (int i = 0; i < elements.size(); i++) {
+				Vector<double> dne = Vector<double>();
+				Vector<double> ane = Vector<double>();
+				Vector<double> lne = Vector<double>();
+				Vector<double> dnm1e = Vector<double>();
+				Vector<double> anm1e = Vector<double>();
+				Vector<double> lnm1e = Vector<double>();
+				for(int j = 0; j < elements[i].size(); j++){
+					dne = dne.Vstack(d[t][elements[i][j]]);
+					ane = ane.Vstack(a[t][elements[i][j]]);
+					lne = lne.Vstack(l[step - t - 1][elements[i][j]]);
+					dnm1e = dnm1e.Vstack(d[t - 1][elements[i][j]]);
+					anm1e = anm1e.Vstack(a[t - 1][elements[i][j]]);
+					lnm1e = lnm1e.Vstack(l[step - t][elements[i][j]]);
+				}
+
+				Matrix<double> Ke, Me;
+				double E = E0*(1.0 - pow(s[i], p)) + E1*pow(s[i], p);
+				double dE = p*(- E0 + E1)*pow(s[i], p - 1.0);
+				double dD = Density*(- rho0 + rho1);
+				PlaneStrain<double, ShapeFunction8Square, Gauss9Square >(Ke, nodes, elements[i], 1.0, Poisson, 1.0);
+				PlaneMass<double, ShapeFunction8Square, Gauss9Square >(Me, nodes, elements[i], 1.0, 1.0);
+
+				objective += 0.5*(0.5*dne*(E*Ke*dne) + 0.5*dnm1e*(E*Ke*dnm1e))*dt;
+				dobjectives[i] += 0.5*(0.5*dne*(dE*Ke*dne) + 0.5*dnm1e*(dE*Ke*dnm1e))*dt + 0.5*(lne*(dD*Me*ane + dE*Ke*dne) + lnm1e*(dD*Me*anm1e + dE*Ke*dnm1e))*dt;
+			}
+		}
+
+
+		//*************************************************
+		//  Post Process
+		//*************************************************
+		std::ofstream fout(model_path + "result" + std::to_string(k) + ".vtk");
 		MakeHeadderToVTK(fout);
 		AddPointsToVTK(nodes, fout);
 		AddElementToVTK(elements, fout);
 		AddElementTypes(std::vector<int>(elements.size(), 23), fout);
-		AddPointVectors(d[t], "d", fout, true);
+		AddElementScalers(s, "s", fout, true);
 		fout.close();
+
+
+		//*************************************************
+        //  Update design variables with OC method
+        //*************************************************
+
+		//----------Check convergence----------
+        std::cout << "Objective:\t" << objective << "\tWeight:\t" << weight << "\t";
+		if(fabs((objective - objectivebefore) / (objective + objectivebefore)) < objectiveeps) {
+			std::cout << std::endl << "----------Convergence----------" << std::endl;
+			break;
+		}
+		
+		//----------Get updated design variables with OC method----------
+		double lambda0 = lambdamin, lambda1 = lambdamax, lambda;
+		std::vector<double> snext = std::vector<double>(elements.size());
+		while((lambda1 - lambda0) / (lambda1 + lambda0) > lambdaeps){
+			lambda = 0.5 * (lambda1 + lambda0);
+
+			for (int i = 0; i < s.size(); i++) {
+				snext[i] = pow(dobjectives[i] / (dweights[i] * lambda), iota) * s[i];
+				if(snext[i] < std::max(0.0, (1.0 - movelimit)*s[i])) {
+					snext[i] = std::max(0.0, (1.0 - movelimit)*s[i]);
+				} else if(snext[i] > std::min(1.0, (1.0 + movelimit)*s[i])) {
+					snext[i] = std::min(1.0, (1.0 + movelimit)*s[i]);
+				}
+			}
+
+			double weightnext = 0.0;
+			for (int i = 0; i < elements.size(); i++) {
+				weightnext += rho0*(1.0 - snext[i]) + rho1*snext[i] - weightlimit;
+			}
+
+			if (weightnext > 0.0) {
+				lambda0 = lambda;
+			}
+			else {
+				lambda1 = lambda;
+			}
+		}
+
+		std::cout << "Lagrange value:\t" << lambda << std::endl;
+
+		//----------Update design variables and objective----------
+		for (int i = 0; i < s.size(); i++) {
+			s[i] = snext[i];
+		}
+		objectivebefore = objective;
 	}
+
+	
 	
 	return 0;
 }

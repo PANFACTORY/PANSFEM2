53430a02a8f80be15270501e94605059bde21d73
PANFACTORY <yutatanabe510@gmail.com>
2019-12-21 01:33:10 +0900

    Update Fluid.h

--- src/FEM/Equation/Fluid.h
+++ src/FEM/Equation/Fluid.h
@@ -22,7 +22,7 @@ namespace PANSFEM2 {
 
     //********************Convective term matrix(Gradient form)********************
     template<class T, template<class>class SF, template<class>class IC>
-	void Convective_GradientForm(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<Vector<T> >& _u, std::vector<int>& _element){
+	void Convective(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<Vector<T> >& _u, std::vector<int>& _element){
         //----------Initialize element stiffness matrix----------
 		_Ke = Matrix<T>(2*_element.size(), 2*_element.size());
 		
@@ -54,25 +54,110 @@ namespace PANSFEM2 {
 			Matrix<T> dNdX = dXdr.Inverse()*dNdr;
 
             //----------Get velocity gradient----------
-            Matrix<T> Z = (dNdX*U).Transpose();
+            Vector<T> u = (U.Transpose())*N;
+			Matrix<T> Z = Matrix<T>(2, 4);
+			Z(0, 0) = u(0);	Z(0, 1) = T();	Z(0, 2) = u(1);	Z(0, 3) = T();
+			Z(1, 0) = T();	Z(1, 1) = u(0);	Z(1, 2) = T();	Z(1, 3) = u(1);
 
-			//----------Generate B matrix----------
-			Matrix<T> B = Matrix<T>(2, 2*_element.size());
+			//----------Generate B and C matrix----------
+			Matrix<T> A = Matrix<T>(2, 2*_element.size());
+			Matrix<T> B = Matrix<T>(4, 2*_element.size());
 			for (int n = 0; n < _element.size(); n++) {
-				B(0, 2*n) = N(n);	B(0, 2*n + 1) = T();		
-				B(1, 2*n) = T();	B(1, 2*n + 1) = N(n);
+				A(0, 2*n) = N(n);	A(0, 2*n + 1) = T();		
+				A(1, 2*n) = T();	A(1, 2*n + 1) = N(n);
+				
+				B(0, 2*n) = dNdX(n, 0);	B(0, 2*n + 1) = T();		
+				B(1, 2*n) = T();		B(1, 2*n + 1) = dNdX(n, 0);
+				B(2, 2*n) = dNdX(n, 1);	B(2, 2*n + 1) = T();		
+				B(3, 2*n) = T();		B(3, 2*n + 1) = dNdX(n, 1);
 			}
 
 			//----------Update element stiffness matrix----------
-			_Ke += B.Transpose()*Z*B*J*IC<T>::Weights[g][0] * IC<T>::Weights[g][1] * IC<T>::Weights[g][2];
+			_Ke += A.Transpose()*Z*B*J*IC<T>::Weights[g][0] * IC<T>::Weights[g][1] * IC<T>::Weights[g][2];
 		}
     }
 
+
     //********************Diffusion term matrix********************
+	template<class T, template<class>class SF, template<class>class IC>
+	void Diffusion(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<int>& _element) {
+		//----------Initialize element matrix----------
+		_Ke = Matrix<T>(2*_element.size(), 2*_element.size());
 
-    //********************Coefficient matrix of Poisson equation for pressure********************
+		//----------Generate cordinate matrix X----------
+		Matrix<T> X = Matrix<T>(_element.size(), 2);
+		for(int i = 0; i < _element.size(); i++){
+			for(int j = 0; j < 2; j++){
+				X(i, j) = _x[_element[i]](j);
+			}
+		}
 
-    //********************Coefficient vector of Poisson equation for pressure********************
+		//----------Loop of Gauss Integration----------
+		for (int g = 0; g < IC<T>::N; g++) {
+			//----------Get difference of shape function----------
+			Matrix<T> dNdr = SF<T>::dNdr(IC<T>::Points[g]);
+
+			//----------Get difference of shape function----------
+			Matrix<T> dXdr = dNdr * X;
+			T J = dXdr.Determinant();
+			Matrix<T> dNdX = dXdr.Inverse() * dNdr;
+
+			//----------Get B matrix----------
+			Matrix<T> B = Matrix<T>(4, 2*_element.size());
+			for (int n = 0; n < _element.size(); n++) {
+				B(0, 2*n) = dNdX(n, 0);	B(0, 2*n + 1) = T();		
+				B(1, 2*n) = T();		B(1, 2*n + 1) = dNdX(n, 0);
+				B(2, 2*n) = dNdX(n, 1);	B(2, 2*n + 1) = T();		
+				B(3, 2*n) = T();		B(3, 2*n + 1) = dNdX(n, 1);
+			}
+
+			//----------Update element stiffness matrix----------
+			_Ke += B.Transpose()*B*J*_alpha*_t*IC<T>::Weights[g][0] * IC<T>::Weights[g][1];
+		}
+	}
+
+
+    //********************Poisson equation for pressure********************
+	template<class T, template<class>class SF, template<class>class IC>
+	void PPoisson(Matrix<T>& _Ke, Vector<T>& _Fe, std::vector<Vector<T> >& _x, std::vector<Vector<T> >& _u, std::vector<int>& _element) {
+		//----------Initialize element matrix----------
+		_Ke = Matrix<T>(_element.size(), _element.size());
+		_Fe = Vector<T>(_element.size());
+
+		//----------Generate cordinate matrix X----------
+		Matrix<T> X = Matrix<T>(_element.size(), 2);
+		for(int i = 0; i < _element.size(); i++){
+			for(int j = 0; j < 2; j++){
+				X(i, j) = _x[_element[i]](j);
+			}
+		}
+
+		//----------Generate cordinate matrix U----------
+		Matrix<T> U = Matrix<T>(_element.size(), 2);
+		for(int i = 0; i < _element.size(); i++){
+			for(int j = 0; j < 2; j++){
+				U(i, j) = _u[_element[i]](j);
+			}
+		}
+
+		//----------Loop of Gauss Integration----------
+		for (int g = 0; g < IC<T>::N; g++) {
+			//----------Get difference of shape function----------
+			Vector<T> N = SF<T>::N(IC<T>::Points[g]);
+			Matrix<T> dNdr = SF<T>::dNdr(IC<T>::Points[g]);
+
+			//----------Get difference of shape function----------
+			Matrix<T> dXdr = dNdr * X;
+			T J = dXdr.Determinant();
+			Matrix<T> B = dXdr.Inverse() * dNdr;
+
+			//----------Update Ke and Fe----------
+			_Ke += B.Transpose()*B*J*_alpha*_t*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
+			_Fe += N.Transpose()
+		}
+	}
+
 
     //********************Coefficient matrix for updating velocity********************
+	
 }

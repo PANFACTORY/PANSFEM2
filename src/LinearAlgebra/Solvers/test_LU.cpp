53e63ec0dab9dbaaaa5a0c9005e9c98f9d84eb49
PANFACTORY <yutatanabe510@gmail.com>
2020-02-14 10:08:30 +0900

    Modify LU decomposition

--- src/LinearAlgebra/Solvers/test_LU.cpp
+++ src/LinearAlgebra/Solvers/test_LU.cpp
@@ -6,19 +6,20 @@
 using namespace PANSFEM2;
 
 int main(){
-    Matrix<double> A = Matrix<double>(3, 3);
-    A(0, 0) = 2.0;  A(0, 1) = 3.0;  A(0, 2) = -1.0;
-    A(1, 0) = 4.0;  A(1, 1) = 4.0;  A(1, 2) = -3.0;
-    A(2, 0) = -2.0; A(2, 1) = 3.0;  A(2, 2) = -1.0;
-    LU(A);
-    std::cout << A << std::endl;
-
-    Matrix<double> B = Matrix<double>(3, 3);
-    B(0, 0) = 3.0;  B(0, 1) = 1.0;  B(0, 2) = 0.0;
-    B(1, 0) = 6.0;  B(1, 1) = 1.0;  B(1, 2) = -2.0;
-    B(2, 0) = -3.0; B(2, 1) = 0.0;  B(2, 2) = 3.0;
-    LU(B);
-    std::cout << B << std::endl;
-
+    Matrix<double> A = Matrix<double>(4, 4);
+    A(0, 0) = 1.0;  A(0, 1) = 1.0;  A(0, 2) = 0.0;  A(0, 3) = 3.0;
+    A(1, 0) = 2.0;  A(1, 1) = 1.0;  A(1, 2) = -1.0; A(1, 3) = 1.0;
+    A(2, 0) = 3.0;  A(2, 1) = -1.0; A(2, 2) = -1.0; A(2, 3) = 2.0;
+    A(3, 0) = -1.0; A(3, 1) = 2.0;  A(3, 2) = 3.0;  A(3, 3) = -1.0;
+    std::vector<int> pivot = std::vector<int>(4);
+    LU(A, pivot);
+    Vector<double> b = Vector<double>(4);
+    b(0) = 4.0;
+    b(1) = 1.0;
+    b(2) = -3.0;
+    b(3) = 4.0;
+    SolveLU(A, b, pivot);
+    std::cout << b << std::endl;
+    
     return 0;
 }

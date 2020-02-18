254701b65249f96885667aaafc17e439cb9591f8
PANFACTORY <yutatanabe510@gmail.com>
2020-02-18 09:38:05 +0900

    Add sample data of shell

---
+++ sample/shell/sample_shell_linear.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+#include "../../src/LinearAlgebra/Models/Vector.h"
+#include "../../src/LinearAlgebra/Models/Matrix.h"
+#include "../../src/LinearAlgebra/Models/LILCSR.h"
+#include "../../src/PrePost/Import/ImportFromCSV.h"
+#include "../../src/FEM/Controller/Assembling.h"
+#include "../../src/FEM/Equation/Shell.h"
+#include "../../src/FEM/Controller/BoundaryCondition.h"
+#include "../../src/LinearAlgebra/Solvers/CG.h"
+#include "../../src/PrePost/Export/ExportToVTK.h"
+#include "../../src/FEM/Controller/ShapeFunction.h"
+#include "../../src/FEM/Controller/IntegrationConstant.h"
+
+
+using namespace PANSFEM2;
+
+
+int main() {
+	//----------Model Path----------
+	std::string model_path = "sample/shell/";
+	
+	//----------Add Nodes----------
+	std::vector<Vector<double> > nodes;
+	ImportNodesFromCSV(nodes, model_path + "Node.csv");
+	
+	//----------Add Elements----------
+	std::vector<std::vector<int> > elements;
+	ImportElementsFromCSV(elements, model_path + "Element.csv");
+
+	//----------Add Field----------
+	std::vector<int> field;
+	int KDEGREE = 0;
+	ImportFieldFromCSV(field, KDEGREE, nodes.size(), model_path + "Field.csv");
+	
+	//----------Add Dirichlet Condition----------
+	std::vector<int> isufixed;
+	std::vector<double> ufixed;
+	ImportDirichletFromCSV(isufixed, ufixed, field, model_path + "Dirichlet.csv");
+
+	//----------Add Neumann Condition----------
+	std::vector<int> isqfixed;
+	std::vector<double> qfixed;
+	ImportNeumannFromCSV(isqfixed, qfixed, field, model_path + "Neumann.csv");
+
+    //----------Generate director vector----------
+    std::vector<Vector<double> > v3 = std::vector<Vector<double> >(nodes.size(), { 0.0, 0.0, 1.0 });
+
+	//----------Culculate Ke, body force and Assembling----------
+	LILCSR<double> K = LILCSR<double>(KDEGREE, KDEGREE);			//System stiffness matrix
+	std::vector<double> F = std::vector<double>(KDEGREE, 0.0);		//External load vector
+	for (auto element : elements) {
+		Matrix<double> Ke;
+		LinearIsotropicElasticShell<double, ShapeFunction4Square, Gauss8Cubic>(Ke, nodes, element, v3, 210000.0, 0.3, 1.0);
+		Assembling(K, Ke, element, field);
+	}
+
+	//----------Set Neumann Boundary Condition----------
+	SetNeumann(F, isqfixed, qfixed);
+
+	//----------Set Dirichlet Boundary Condition----------
+	SetDirichlet(K, F, isufixed, ufixed, 1.0e10);
+
+	//----------Solve System Equation----------
+	CSR<double> Kmod = CSR<double>(K);
+	std::vector<double> result = ScalingCG(Kmod, F, 100000, 1.0e-10);
+
+	//----------Update displacement u----------
+	std::vector<Vector<double> > ur = std::vector<Vector<double> >(nodes.size(), Vector<double>(5));
+	FieldResultToNodeValue(result, ur, field);
+    std::vector<Vector<double> > u = std::vector<Vector<double> >(nodes.size());
+    for(int i = 0; i < nodes.size(); i++){
+        u[i] = ur[i].Segment(0, 3);
+    }
+			
+	//----------Save initial value----------
+	std::ofstream fout(model_path + "result.vtk");
+	MakeHeadderToVTK(fout);
+	AddPointsToVTK(nodes, fout);
+	AddElementToVTK(elements, fout);
+	std::vector<int> et = std::vector<int>(elements.size(), 5);
+	AddElementTypes(et, fout);
+	AddPointVectors(u, "u", fout, true);
+	fout.close();
+
+	return 0;
+}
--- src/FEM/Equation/Shell.h
+++ src/FEM/Equation/Shell.h
@@ -18,7 +18,7 @@
 namespace PANSFEM2 {
     //********************Linear Isotropic Elastic Shell********************
 	template<class T, template<class>class SF, template<class>class IC>
-	void LinearIsotropicElasticShell(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<Vector<T> >& _v, std::vector<int>& _element, T _E, T _V, T _t) {
+	void LinearIsotropicElasticShell(Matrix<T>& _Ke, std::vector<Vector<T> >& _x, std::vector<int>& _element, std::vector<Vector<T> >& _v, T _E, T _V, T _t) {
 		//----------Initialize element stiffness matrix----------
 		_Ke = Matrix<T>(5*_element.size(), 5*_element.size());
 		
@@ -35,11 +35,11 @@ namespace PANSFEM2 {
         Matrix<T> v2 = Matrix<T>(0, 3);
         for(auto i : _element){
             Vector<T> v3i = _v[i].Normal();
-            v3 = v3.VStack(v3i.Transpose());
+            v3 = v3.Vstack(v3i.Transpose());
             Vector<T> v1i = VectorProduct(l, v3i).Normal();
-            v1 = v1.VStack(v1i.Transpose());
+            v1 = v1.Vstack(v1i.Transpose());
             Vector<T> v2i = VectorProduct(v3i, v1i).Normal();
-            v2 = v2.VStack(v2i.Transpose());
+            v2 = v2.Vstack(v2i.Transpose());
         }
 
         //----------Generate local D matrix----------
@@ -55,12 +55,13 @@ namespace PANSFEM2 {
 		//----------Loop of Gauss Integration----------
 		for (int g = 0; g < IC<T>::N; g++) {
 			//----------Get difference of shape function----------
-            Vector<T> N = SF<T>::N(IC<T>::Points[g].Segment(0, 2));
-			Matrix<T> dNdr = SF<T>::dNdr(IC<T>::Points[g].Segment(0, 2));
-
+            Vector<T> gausspoint = IC<T>::Points[g];
+            Vector<T> N = SF<T>::N(gausspoint.Segment(0, 2));
+			Matrix<T> dNdr = SF<T>::dNdr(gausspoint.Segment(0, 2));
+            
 			//----------Generate Jacobi matrix and derivative----------
-            T zeta = IC<T>::Points[g].Segment(2, 1)(0);
-			Matrix<T> J = (dNdr*(X + 0.5*_t*zeta*v3)).VStack(0.5*_t*N.Transpose()*v3);
+            T zeta = gausspoint.Segment(2, 3)(0);
+			Matrix<T> J = (dNdr*(X + 0.5*_t*zeta*v3)).Vstack(0.5*_t*N.Transpose()*v3);
             T detJ = J.Determinant();
             Matrix<T> invJ = J.Inverse();
             Matrix<T> dNdx = invJ.Block(0, 0, 3, 2)*dNdr;
@@ -92,7 +93,7 @@ namespace PANSFEM2 {
             Matrix<T> D = P.Transpose()*D0*P;
             
             //----------Update element stiffness matrix----------
-            _Ke += B.Transpose()*D*B*detJ*IC::Weights[g][0]*IC::Weights[g][1]*IC::Weights[g][2];
+            _Ke += B.Transpose()*D*B*detJ*IC<T>::Weights[g][0]*IC<T>::Weights[g][1]*IC<T>::Weights[g][2];
 		}
 	}
 }

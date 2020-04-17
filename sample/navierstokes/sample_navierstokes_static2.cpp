d76ea8c709e671c01f983a78727359fa1208608b
PANFACTORY <yutatanabe510@gmail.com>
2020-04-17 17:29:33 +0900

    Modify NavierStokes

---
+++ sample/navierstokes/sample_navierstokes_static2.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+
+
+#include "../../src/LinearAlgebra/Models/Vector.h"
+#include "../../src/PrePost/Import/ImportFromCSV.h"
+#include "../../src/FEM/Equation/Stokes.h"
+#include "../../src/FEM/Equation/NavierStokes.h"
+#include "../../src/FEM/Controller/ShapeFunction.h"
+#include "../../src/FEM/Controller/GaussIntegration.h"
+#include "../../src/FEM/Controller/BoundaryCondition2.h"
+#include "../../src/FEM/Controller/Assembling2.h"
+#include "../../src/LinearAlgebra/Solvers/CG.h"
+#include "../../src/PrePost/Export/ExportToVTK.h"
+
+
+
+using namespace PANSFEM2;
+
+
+int main() {
+    std::string model_path = "sample/navierstokes/model2/";
+	std::vector<Vector<double> > x;
+	ImportNodesFromCSV(x, model_path + "Node.csv");
+	std::vector<std::vector<int> > elementsu;
+	ImportElementsFromCSV(elementsu, model_path + "ElementU.csv");
+	std::vector<std::vector<int> > elementsp;
+	ImportElementsFromCSV(elementsp, model_path + "ElementP.csv");
+	std::vector<std::vector<int> > edgesu;
+	ImportElementsFromCSV(edgesu, model_path + "EdgeU.csv");
+	std::vector<std::vector<int> > edgesp;
+	ImportElementsFromCSV(edgesp, model_path + "EdgeP.csv");
+    std::vector<std::pair<std::pair<int, int>, double> > ufixed;
+	ImportDirichletFromCSV(ufixed, model_path + "Dirichlet.csv");
+
+    std::vector<Vector<double> > up = std::vector<Vector<double> >(x.size(), Vector<double>(3));
+	std::vector<std::vector<int> > nodetoglobal = std::vector<std::vector<int> >(x.size(), std::vector<int>(3, 0));
+	
+    int kmax = 10;
+	std::vector<std::pair<std::pair<int, int>, double> > ufixed0 = ufixed;
+    for(auto& ufixedi : ufixed0) {
+        ufixedi.second *= 1.0/(double)kmax;
+    }
+    SetDirichlet(up, nodetoglobal, ufixed0);
+	int KDEGREE = Renumbering(nodetoglobal);
+
+    //----------Get initial result with Stokes equation----------
+    LILCSR<double> K = LILCSR<double>(KDEGREE, KDEGREE); 
+    std::vector<double> F = std::vector<double>(KDEGREE, 0.0);
+
+    for (int i = 0; i < elementsu.size(); i++) {
+        std::vector<std::vector<std::pair<int, int> > > nodetoelementu;
+        std::vector<std::vector<std::pair<int, int> > > nodetoelementp;
+        Matrix<double> Ke;
+        Stokes<double, ShapeFunction6Triangle, ShapeFunction3Triangle, Gauss3Triangle>(Ke, nodetoelementu, elementsu[i], nodetoelementp, elementsp[i], { 0, 1, 2 }, x, 1.0);
+        Assembling(K, F, up, Ke, nodetoglobal, nodetoelementu, elementsu[i], nodetoelementu, elementsu[i]);
+        Assembling(K, F, up, Ke, nodetoglobal, nodetoelementu, elementsu[i], nodetoelementp, elementsp[i]);
+        Assembling(K, F, up, Ke, nodetoglobal, nodetoelementp, elementsp[i], nodetoelementu, elementsu[i]);
+        Assembling(K, F, up, Ke, nodetoglobal, nodetoelementp, elementsp[i], nodetoelementp, elementsp[i]);
+    }
+
+    CSR<double> Kmod = CSR<double>(K);
+    std::vector<double> result = CG(Kmod, F, 100000, 1.0e-10);
+    Disassembling(up, result, nodetoglobal);
+    
+    //----------Incremental step loop----------
+    for(int k = 1; k < 2; k++) {
+        std::cout << "k=" << k;
+
+        std::vector<std::pair<std::pair<int, int>, double> > ufixedk = ufixed;
+        for(auto& ufixedi : ufixedk) {
+            ufixedi.second *= (k + 1)/(double)kmax;
+        }
+        SetDirichlet(up, nodetoglobal, ufixedk);
+
+        //----------Newton-Raphson loop----------
+        for(int l = 0; l < 0; l++) {
+            LILCSR<double> K = LILCSR<double>(KDEGREE, KDEGREE);			//System stiffness matrix
+            std::vector<double> R = std::vector<double>(KDEGREE, 0.0);		//Residual load vector
+
+            for (int i = 0; i < elementsu.size(); i++) {
+                std::vector<std::vector<std::pair<int, int> > > nodetoelementu;
+                std::vector<std::vector<std::pair<int, int> > > nodetoelementp;
+
+                Matrix<double> Ke;
+                Vector<double> Qe;
+                NavierStokes<double, ShapeFunction6Triangle, ShapeFunction3Triangle, Gauss3Triangle>(Ke, Qe, nodetoelementu, elementsu[i], nodetoelementp, elementsp[i], { 0, 1, 2 }, x, up, 1.0);
+            
+                Assembling(K, R, up, Ke, nodetoglobal, nodetoelementu, elementsu[i], nodetoelementu, elementsu[i]);
+                Assembling(K, R, up, Ke, nodetoglobal, nodetoelementu, elementsu[i], nodetoelementp, elementsp[i]);
+                Assembling(K, R, up, Ke, nodetoglobal, nodetoelementp, elementsp[i], nodetoelementu, elementsu[i]);
+                Assembling(K, R, up, Ke, nodetoglobal, nodetoelementp, elementsp[i], nodetoelementp, elementsp[i]); 
+
+                Assembling(R, Qe, nodetoglobal, nodetoelementu, elementsu[i]);
+                Assembling(R, Qe, nodetoglobal, nodetoelementp, elementsp[i]);  
+            }
+  
+            double normR = std::inner_product(R.begin(), R.end(), R.begin(), 0.0);
+            if (normR < 1.0e-1) {
+                std::cout << "\tConvergence at l = " << l << "\tR Norm = " << normR << std::endl;
+                break;
+            }
+            
+            CSR<double> Kmod = CSR<double>(K);
+            std::vector<double> result = BiCGSTAB(Kmod, R, 100000, 1.0e-10);
+            std::vector<Vector<double> > dup = std::vector<Vector<double> >(x.size(), Vector<double>(3));
+            Disassembling(dup, result, nodetoglobal);
+            for(int i = 0; i < x.size(); i++){
+                up[i] += dup[i];
+            }
+        }
+    }
+
+    std::vector<Vector<double> > u = std::vector<Vector<double> >(x.size());
+    std::vector<double> p = std::vector<double>(x.size(), 0.0);
+    for(int i = 0; i < x.size(); i++){
+        u[i] = up[i].Segment(0, 2);
+        p[i] = up[i](2);
+    }
+
+    std::ofstream fout(model_path + "_result.vtk");
+    MakeHeadderToVTK(fout);
+    AddPointsToVTK(x, fout);
+    AddElementToVTK(elementsp, fout);
+    AddElementTypes(std::vector<int>(elementsp.size(), 5), fout);
+    AddPointVectors(u, "u", fout, true);
+    AddPointScalers(p, "p", fout, false);
+    fout.close();
+    
+	return 0;
+}
--- src/FEM/Equation/NavierStokes.h
+++ src/FEM/Equation/NavierStokes.h
@@ -1,8 +1,8 @@
 //*****************************************************************************
-//  Title       :src/FEM/Equation/NavierStokes.h
-//  Author      :Tanabe Yuta
-//  Date        :2020/02/10
-//  Copyright   :(C)2019 TanabeYuta
+//  Title       :	src/FEM/Equation/NavierStokes.h
+//  Author      :	Tanabe Yuta
+//  Date        :	2020/02/10
+//  Copyright   :	(C)2019 TanabeYuta
 //*****************************************************************************
 
 
@@ -136,6 +136,100 @@ namespace PANSFEM2 {
             for(int i = n; i < m; i++){
                 F(n + 2*i)     = M(i)*U(0)*dUdX(0, 0) + M(i)*U(1)*dUdX(1, 0) + 2.0*_nu*dMdX(0, i)*dUdX(0, 0) + _nu*dMdX(1, i)*dUdX(1, 0) + _nu*dMdX(1, i)*dUdX(0, 1) - dMdX(0, i)*P;
 				F(n + 2*i + 1) = M(i)*U(0)*dUdX(0, 1) + M(i)*U(1)*dUdX(1, 1) + _nu*dMdX(0, i)*dUdX(1, 0) + _nu*dMdX(0, i)*dUdX(0, 1) + 2.0*_nu*dMdX(1, i)*dUdX(1, 1) - dMdX(1, i)*P;
+            }
+			_Fe -= F*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
+		}
+	}
+
+
+
+
+
+
+
+
+
+	//******************************Get element stiffness matrix******************************
+	template<class T, template<class>class SFU, template<class>class SFP, template<class>class IC>
+	void NavierStokes(Matrix<T>& _Ke, Vector<T>& _Fe, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementu, const std::vector<int>& _elementu, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementp, const std::vector<int>& _elementp, const std::vector<int>& _doulist, std::vector<Vector<T> >& _x, std::vector<Vector<T> >& _up, T _nu) {
+		assert(_doulist.size() == 3);
+
+		int m = _elementu.size();   //  Number of shapefunction for velosity u
+        int n = _elementp.size();   //  Number of shapefunction for pressure p
+
+		_Ke = Matrix<T>(2*m + n, 2*m + n);
+		_Fe = Vector<T>(2*m + n);
+		_nodetoelementu = std::vector<std::vector<std::pair<int, int> > >(m, std::vector<std::pair<int, int> >(2));
+		for(int i = 0; i < m; i++) {
+			_nodetoelementu[i][0] = std::make_pair(_doulist[0], i);
+			_nodetoelementu[i][1] = std::make_pair(_doulist[1], m + i);
+		}
+        _nodetoelementp = std::vector<std::vector<std::pair<int, int> > >(n, std::vector<std::pair<int, int> >(1));
+		for(int i = 0; i < n; i++) {
+			_nodetoelementp[i][0] = std::make_pair(_doulist[2], 2*m + i);
+		}
+
+		Matrix<T> Xu = Matrix<T>(m, 2);
+		for(int i = 0; i < m; i++){
+			Xu(i, 0) = _x[_elementu[i]](0); Xu(i, 1) = _x[_elementu[i]](1);
+		}
+
+        Matrix<T> Xp = Matrix<T>(n, 2);
+		for(int i = 0; i < n; i++){
+			Xp(i, 0) = _x[_elementp[i]](0); Xp(i, 1) = _x[_elementp[i]](1);
+		}
+
+		Matrix<T> u = Matrix<T>(m, 2);
+		for(int i = 0; i < m; i++){
+			u(i, 0) = _up[_elementu[i]](0); u(i, 1) = _up[_elementu[i]](1);
+		}
+
+		Vector<T> p = Vector<T>(n);
+		for(int i = 0; i < n; i++){
+			p(i) = _up[_elementp[i]](2);
+		}
+
+		for (int g = 0; g < IC<T>::N; g++) {
+			Vector<T> N = SFP<T>::N(IC<T>::Points[g]);
+			Matrix<T> dNdr = SFP<T>::dNdr(IC<T>::Points[g]);
+			Matrix<T> dXdr = dNdr*Xp;
+			T J = dXdr.Determinant();
+			Matrix<T> dNdX = dXdr.Inverse()*dNdr;
+
+			Vector<T> M = SFU<T>::N(IC<T>::Points[g]);
+			Matrix<T> dMdr = SFU<T>::dNdr(IC<T>::Points[g]);
+			Matrix<T> dMdX = dXdr.Inverse()*dMdr;
+
+			Vector<T> U = u.Transpose()*M;
+			Matrix<T> dUdX = dMdX*u;
+
+			T P = p*N;
+			Vector<T> dpdX = dNdX*p;
+
+            Matrix<T> K = Matrix<T>(2*m + n, 2*m + n);
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < m; j++){
+                    K(i, j) = M(i)*M(j)*dUdX(0, 0) + M(i)*dMdX(0, j)*U(0) + M(i)*dMdX(1, j)*U(1) + 2.0*_nu*dMdX(0, i)*dMdX(0, j) + _nu*dMdX(1, i)*dMdX(1, j) + _nu*dMdX(1, i)*dMdX(0, j);	
+					K(i, j + m) = M(i)*M(j)*dUdX(1, 0);																	
+                    K(i + m, j) = M(i)*M(j)*dUdX(0, 1);																		
+					K(i + m, j + m) = M(i)*M(j)*dUdX(1, 1) + M(i)*dMdX(1, j)*U(1) + M(i)*dMdX(0, j)*U(0) + _nu*dMdX(0, i)*dMdX(1, j) + _nu*dMdX(0, i)*dMdX(0, j) + 2.0*_nu*dMdX(1, i)*dMdX(1, j);	
+                }
+                for(int j = 0; j < n; j++){
+					K(i, j + 2*m) = -N(j)*dMdX(0, i);
+					K(i + m, j + 2*m) = -N(j)*dMdX(1, i);
+                    K(j + 2*m, i) = N(j)*dMdX(0, i);                                         						
+					K(j + 2*m, i + m) = N(j)*dMdX(1, i);
+                }
+            }
+			_Ke += K*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
+
+			Vector<T> F = Vector<T>(2*m + n);
+            for(int i = 0; i < m; i++){
+				F(i)     = M(i)*U(0)*dUdX(0, 0) + M(i)*U(1)*dUdX(1, 0) + 2.0*_nu*dMdX(0, i)*dUdX(0, 0) + _nu*dMdX(1, i)*dUdX(1, 0) + _nu*dMdX(1, i)*dUdX(0, 1) - dMdX(0, i)*P;
+				F(i + m) = M(i)*U(0)*dUdX(0, 1) + M(i)*U(1)*dUdX(1, 1) + _nu*dMdX(0, i)*dUdX(1, 0) + _nu*dMdX(0, i)*dUdX(0, 1) + 2.0*_nu*dMdX(1, i)*dUdX(1, 1) - dMdX(1, i)*P;
+            }
+            for(int i = 0; i < n; i++){
+                F(i + 2*m) = N(i)*dUdX(0, 0) + N(i)*dUdX(1, 1);
             }
 			_Fe -= F*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
 		}

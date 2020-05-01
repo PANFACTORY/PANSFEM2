c82e4dcc108e6da43ee9e90f14df3600db83ab64
PANFACTORY <yutatanabe510@gmail.com>
2020-05-01 11:25:48 +0900

    Modify Square mesher

--- src/PrePost/Mesher/test_Mesher.cpp
+++ src/PrePost/Mesher/test_Mesher.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 
-#include "SquareAnnulusMesh.h"
+#include "SquareMesh.h"
 #include "../Export/ExportToVTK.h"
 
 
@@ -10,23 +10,26 @@ using namespace PANSFEM2;
 
 
 int main(){
-    SquareAnnulusMesh<double> mesh = SquareAnnulusMesh<double>(1.0, 2.0, 3.0, 4.0, 2, 5, 3);
+    SquareMesh<double> mesh = SquareMesh<double>(5.0, 4.0, 5, 4);
     std::vector<Vector<double> > nodes = mesh.GenerateNodes();
     std::vector<std::vector<int> > elements = mesh.GenerateElements();
     std::vector<std::vector<int> > edges = mesh.GenerateEdges();
-    std::vector<int> field = mesh.GenerateFields(2);
     /*std::vector<int> isufixed = mesh.GenerateFixedlist(2, { 0, 1 }, [](Vector<double> _x){
         if(_x.Norm() < 1.0 + 1.0e-5) {
             return true;
         }
         return false;
     });*/
-
-    for(auto edgesi : edges) {
-        for(auto edgesij : edgesi) {
-            std::cout << edgesij << "\t";
+    std::vector<int> elements0 = mesh.GenerateElementIdsSelected([](Vector<double> _x){
+        double eps = 1.0e-5;
+        if(2.0 - eps < _x(0) && _x(0) < 4.0 + eps && 1.0 - eps < _x(1) && _x(1) < 3.0 + eps) {
+            return true;
         }
-        std::cout << std::endl;    
+        return false;
+    });
+
+    for(auto i : elements0) {
+        std::cout << i << std::endl;    
     }
 
     std::ofstream fout("result.vtk");

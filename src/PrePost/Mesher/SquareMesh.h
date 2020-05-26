d0f6dc2419f5aaa8ffad062f27c1cf9efeff2b97
PANFACTORY <yutatanabe510@gmail.com>
2020-05-26 16:06:24 +0900

    Modify mesher

--- src/PrePost/Mesher/SquareMesh.h
+++ src/PrePost/Mesher/SquareMesh.h
@@ -24,12 +24,17 @@ public:
 
 
         std::vector<Vector<T> > GenerateNodes();
+        std::vector<Vector<T> > GenerateNodes2();
         std::vector<std::vector<int> > GenerateElements();
+        std::vector<std::vector<int> > GenerateElements2();
         template<class F>
         std::vector<int> GenerateElementIdsSelected(F _iscorrespond);
         std::vector<std::vector<int> > GenerateEdges();
+        std::vector<std::vector<int> > GenerateEdges2();
         template<class F>
         std::vector<std::pair<std::pair<int, int>, T> > GenerateFixedlist(std::vector<int> _ulist, F _iscorrespond);
+        template<class F>
+        std::vector<std::pair<std::pair<int, int>, T> > GenerateFixedlist2(std::vector<int> _ulist, F _iscorrespond);
 private:
         T x, y;
         int nx, ny;  
@@ -62,11 +67,47 @@ private:
 
 
     template<class T>
+    std::vector<Vector<T> > SquareMesh<T>::GenerateNodes2(){
+        std::vector<Vector<T> > nodes = std::vector<Vector<T> >((2.0*this->nx + 1)*(2.0*this->ny + 1) - this->nx*this->ny);
+        //----------Corner nodes----------
+        for(int i = 0; i < this->nx + 1; i++) {
+            for(int j = 0; j < this->ny + 1; j++) {
+                nodes[(this->ny + 1)*i + j] = { this->x*(i/(T)this->nx), this->y*(j/(T)this->ny) };
+            }
+        }
+        //----------Middle nodes----------
+        for(int i = 0; i < this->nx + 1; i++) {
+            for(int j = 0; j < this->ny; j++) {
+                nodes[this->ny*i + j + (this->nx + 1)*(this->ny + 1)] = { this->x*(i/(T)this->nx), this->y*((j + 0.5)/(T)this->ny) };
+            } 
+        }
+        for(int i = 0; i < this->nx; i++) {
+            for(int j = 0; j < this->ny + 1; j++) {
+                nodes[(this->ny + 1)*i + j + (this->nx + 1)*(2.0*this->ny + 1)] = { this->x*((i + 0.5)/(T)this->nx), this->y*(j/(T)this->ny) };
+            }
+        }
+        return nodes;
+    }
+
+
+    template<class T>
     std::vector<std::vector<int> > SquareMesh<T>::GenerateElements(){
         std::vector<std::vector<int> > elements = std::vector<std::vector<int> >(this->nx*this->ny);
         for(int i = 0; i < this->nx; i++){
             for(int j = 0; j < this->ny; j++){
                 elements[this->ny*i + j] = { (this->ny + 1)*i + j, (this->ny + 1)*(i + 1) + j, (this->ny + 1)*(i + 1) + (j + 1), (this->ny + 1)*i + (j + 1) };
+            }
+        }
+        return elements;
+    }
+
+
+    template<class T>
+    std::vector<std::vector<int> > SquareMesh<T>::GenerateElements2(){
+        std::vector<std::vector<int> > elements = std::vector<std::vector<int> >(this->nx*this->ny);
+        for(int i = 0; i < this->nx; i++){
+            for(int j = 0; j < this->ny; j++){
+                elements[this->ny*i + j] = { (this->ny + 1)*i + j, (this->ny + 1)*(i + 1) + j, (this->ny + 1)*(i + 1) + (j + 1), (this->ny + 1)*i + (j + 1), (this->ny + 1)*i + j + (this->nx + 1)*(2*this->ny + 1), this->ny*(i + 1) + j + (this->nx + 1)*(this->ny + 1), (this->ny + 1)*i + (j + 1) + (this->nx + 1)*(2*this->ny + 1), this->ny*i + j + (this->nx + 1)*(this->ny + 1) };
             }
         }
         return elements;
@@ -105,6 +146,21 @@ private:
 
 
     template<class T>
+    std::vector<std::vector<int> > SquareMesh<T>::GenerateEdges2() {
+        std::vector<std::vector<int> > edges = std::vector<std::vector<int> >(2*(this->nx + this->ny));
+        for(int i = 0; i < this->nx; i++) {
+            edges[i] = { (this->ny + 1)*i, (this->ny + 1)*(i + 1), (this->ny + 1)*i + (this->nx + 1)*(2*this->ny + 1) };
+            edges[2*this->nx + this->ny - i - 1] = { (this->ny + 1)*(i + 1) + this->ny, (this->ny + 1)*i  + this->ny, (this->ny + 1)*i + (this->nx + 1)*(2*this->ny + 1) + this->ny };
+        }
+        for(int j = 0; j < this->ny; j++) {
+            edges[j + this->nx] = { (this->ny + 1)*this->nx + j, (this->ny + 1)*this->nx + j + 1, j + (this->nx + 1)*(this->ny + 1) + this->nx*this->ny };
+            edges[2*(this->nx + this->ny) - j - 1] = { j + 1, j, j + (this->nx + 1)*(this->ny + 1) };
+        }
+        return edges;
+    }
+
+
+    template<class T>
     template<class F>
     std::vector<std::pair<std::pair<int, int>, T> > SquareMesh<T>::GenerateFixedlist(std::vector<int> _ulist, F _iscorrespond) {
         assert(0 <= *std::min_element(_ulist.begin(), _ulist.end()));
@@ -114,6 +170,44 @@ private:
                 if(_iscorrespond(Vector<T>({ this->x*(i/(T)this->nx), this->y*(j/(T)this->ny) }))) {
                     for(auto ui : _ulist) {
                         ufixed.push_back({ { (this->ny + 1)*i + j, ui }, T() });
+                    }
+                }
+            }
+        }
+        return ufixed;
+    }
+
+
+    template<class T>
+    template<class F>
+    std::vector<std::pair<std::pair<int, int>, T> > SquareMesh<T>::GenerateFixedlist2(std::vector<int> _ulist, F _iscorrespond) {
+        assert(0 <= *std::min_element(_ulist.begin(), _ulist.end()));
+        std::vector<std::pair<std::pair<int, int>, T> > ufixed;
+        //----------Corner nodes----------
+        for(int i = 0; i < this->nx + 1; i++){
+            for(int j = 0; j < this->ny + 1; j++){
+                if(_iscorrespond(Vector<T>({ this->x*(i/(T)this->nx), this->y*(j/(T)this->ny) }))) {
+                    for(auto ui : _ulist) {
+                        ufixed.push_back({ { (this->ny + 1)*i + j, ui }, T() });
+                    }
+                }
+            }
+        }
+        //----------Middle nodes----------
+        for(int i = 0; i < this->nx + 1; i++) {
+            for(int j = 0; j < this->ny; j++) {
+                if(_iscorrespond(Vector<T>({ this->x*(i/(T)this->nx), this->y*((j + 0.5)/(T)this->ny) }))) {
+                    for(auto ui : _ulist) {
+                        ufixed.push_back({ { this->ny*i + j + (this->nx + 1)*(this->ny + 1), ui }, T() });
+                    }
+                }
+            } 
+        }
+        for(int i = 0; i < this->nx; i++) {
+            for(int j = 0; j < this->ny + 1; j++) {
+                if(_iscorrespond(Vector<T>({ this->x*((i + 0.5)/(T)this->nx), this->y*(j/(T)this->ny) }))) {
+                    for(auto ui : _ulist) {
+                        ufixed.push_back({ { (this->ny + 1)*i + j + (this->nx + 1)*(2.0*this->ny + 1), ui }, T() });
                     }
                 }
             }
--- src/PrePost/Mesher/test_Mesher.cpp
+++ src/PrePost/Mesher/test_Mesher.cpp
@@ -13,10 +13,10 @@ using namespace PANSFEM2;
 
 
 int main(){
-    SquareMesh2<double> mesh = SquareMesh2<double>(1.0, 1.0, 7, 20, 0.8, 1.2);
-    std::vector<Vector<double> > nodes = mesh.GenerateNodes();
-    std::vector<std::vector<int> > elements = mesh.GenerateElements();
-    std::vector<std::vector<int> > edges = mesh.GenerateEdges();
+    SquareMesh<double> mesh = SquareMesh<double>(4.0, 3.0, 4, 3);
+    std::vector<Vector<double> > nodes = mesh.GenerateNodes2();
+    std::vector<std::vector<int> > elements = mesh.GenerateElements2();
+    std::vector<std::vector<int> > edges = mesh.GenerateEdges2();
     /*std::vector<int> isufixed = mesh.GenerateFixedlist(2, { 0, 1 }, [](Vector<double> _x){
         if(_x.Norm() < 1.0 + 1.0e-5) {
             return true;
@@ -29,20 +29,20 @@ int main(){
         area += Area<double, ShapeFunction4Square, Gauss4Square>(nodes, element);
     }
     std::cout << area << std::endl;
-    
+    */
 
     for(auto edge : edges) {
         for(auto nodeid : edge) {
             std::cout << nodeid << "\t";
         }
         std::cout << std::endl;    
-    }*/
+    }
 
     std::ofstream fout("result.vtk");
     MakeHeadderToVTK(fout);
     AddPointsToVTK(nodes, fout);
     AddElementToVTK(elements, fout);
-    AddElementTypes(std::vector<int>(elements.size(), 5), fout);
+    AddElementTypes(std::vector<int>(elements.size(), 23), fout);
     fout.close();
     return 0;
 }

9bf420836b3a3cb8ec2e0a3d8a3685c03f22b323
PANFACTORY <yutatanabe510@gmail.com>
2020-06-27 16:07:48 +0900

    Update HeatTransfer.h

--- src/FEM/Equation/HeatTransfer.h
+++ src/FEM/Equation/HeatTransfer.h
@@ -69,4 +69,32 @@ namespace PANSFEM2 {
 			_Ce += N*N.Transpose()*J*_rho*_c*_t*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
 		}
 	}
+
+
+	//******************************Make surface flux vector******************************
+	template<class T, template<class>class SF, template<class>class IC, class F>
+	void HeatTransferSurfaceFlux(Vector<T>& _Fe, std::vector<std::vector<std::pair<int, int> > >& _nodetoelement, const std::vector<int>& _element, const std::vector<int>& _doulist, std::vector<Vector<T> >& _x, F _f, T _t){
+		assert(_doulist.size() == 1);
+
+		_Fe = Vector<T>(_element.size());
+		_nodetoelement = std::vector<std::vector<std::pair<int, int> > >(_element.size(), std::vector<std::pair<int, int> >(1));
+		for(int i = 0; i < _element.size(); i++) {
+			_nodetoelement[i][0] = std::make_pair(_doulist[0], i);
+		}
+
+		Matrix<T> X = Matrix<T>(_element.size(), 2);
+		for(int i = 0; i < _element.size(); i++){
+			X(i, 0) = _x[_element[i]](0);
+			X(i, 1) = _x[_element[i]](1);
+		}
+
+		for (int g = 0; g < IC<T>::N; g++) {
+			Matrix<T> N = SF<T>::N(IC<T>::Points[g]);
+			Matrix<T> dNdr = SF<T>::dNdr(IC<T>::Points[g]);
+			Vector<T> x = X.Transpose()*N;
+			Matrix<T> dXdr = dNdr*X;
+			T dl = sqrt((dXdr*dXdr.Transpose())(0, 0));
+			_Fe += N*_f(x)*dl*_t*IC<T>::Weights[g][0];
+		}
+	}
 }

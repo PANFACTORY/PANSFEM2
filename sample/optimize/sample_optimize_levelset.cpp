194052e1dc4783a826ed662a54d4010d1441e7db
PANFACTORY <yutatanabe510@gmail.com>
2020-07-02 15:39:43 +0900

    Update sample_optimize_levelset.cpp

--- sample/optimize/sample_optimize_levelset.cpp
+++ sample/optimize/sample_optimize_levelset.cpp
@@ -94,48 +94,16 @@ int main() {
 
         CSR<double> Kmod = CSR<double>(K);	
         std::vector<double> result = ScalingCG(Kmod, F, 100000, 1.0e-10);
-        Disassembling(u, result, nodetoglobal);
-
-        RemoveBoundaryConditions(nodetoglobal);
-        KDEGREE = Renumbering(nodetoglobal);
-        std::vector<double> RF = std::vector<double>(KDEGREE, 0.0);
-        std::vector<Vector<double> > r = std::vector<Vector<double> >(x.size(), Vector<double>(2));	
-
-        for (int i = 0; i < elements.size(); i++) {
-			std::vector<std::vector<std::pair<int, int> > > nodetoelement;
-            Matrix<double> Ke;
-            PlaneStressStiffness<double, ShapeFunction4Square, Gauss4Square>(Ke, nodetoelement, elements[i], { 0, 1 }, x, Emin + str[i]*(E0 - Emin), nu, 1.0);
-            Vector<double> Keue = Ke*ElementVector(u, nodetoelement, elements[i]);
-            Assembling(RF, Keue, nodetoglobal, nodetoelement, elements[i]);
-        }
-
-        Disassembling(r, RF, nodetoglobal);
+        Disassembling(u, result, nodetoglobal);     
 
 
-        //----------目的汎関数と制約汎関数の計算----------
-        objective[t] = std::inner_product(u.begin(), u.end(), r.begin(), 0.0);
-        double vol = std::accumulate(str.begin(), str.end(), 0.0)/(double)elements.size();
-
-
-        //----------収束条件の判定とファイル出力----------
-        if(t > nvol && fabs(vol - Vmax) < 0.005 && 
-            fabs(objective[t] - objective[t - 5]) < 0.01*fabs(objective[t]) && 
-            fabs(objective[t] - objective[t - 4]) < 0.01*fabs(objective[t]) && 
-            fabs(objective[t] - objective[t - 3]) < 0.01*fabs(objective[t]) &&
-            fabs(objective[t] - objective[t - 2]) < 0.01*fabs(objective[t]) &&
-            fabs(objective[t] - objective[t - 1]) < 0.01*fabs(objective[t])) {
-            
-            std::cout << "----------Convergence----------" << std::endl;
-            break;
-        }
-
+        //----------ファイル出力----------
         std::ofstream fout("sample/optimize/result" + std::to_string(t) + ".vtk");
 		MakeHeadderToVTK(fout);
 		AddPointsToVTK(x, fout);
 		AddElementToVTK(elements, fout);
 		AddElementTypes(std::vector<int>(elements.size(), 9), fout);
 		AddPointVectors(u, "u", fout, true);
-        AddPointVectors(r, "r", fout, false);
         AddPointScalers(phi, "phi", fout, false);
 		AddElementScalers(str, "str", fout, true);
 		fout.close();
@@ -147,8 +115,10 @@ int main() {
         for (int i = 0; i < elements.size(); i++) {
 			std::vector<std::vector<std::pair<int, int> > > nodetoelement;
             Matrix<double> Ke;
-            PlaneStressStiffness<double, ShapeFunction4Square, Gauss4Square>(Ke, nodetoelement, elements[i], { 0, 1 }, x, (A1 + 2.0*A2)*(1.0 - pow(c, 2.0)), c, 1.0);
+            PlaneStressStiffness<double, ShapeFunction4Square, Gauss4Square>(Ke, nodetoelement, elements[i], { 0, 1 }, x, Emin + str[i]*(E0 - Emin), nu, 1.0);
             Vector<double> ue = ElementVector(u, nodetoelement, elements[i]);
+            objective[t] += ue*(Ke*ue);
+            PlaneStressStiffness<double, ShapeFunction4Square, Gauss4Square>(Ke, nodetoelement, elements[i], { 0, 1 }, x, (A1 + 2.0*A2)*(1.0 - pow(c, 2.0)), c, 1.0);
             for(int j = 0; j < elements[i].size(); j++) {
                 TDN[elements[i][j]](0) += (1.0e-4 + str[i]*(1.0 - 1.0e-4))*ue*(Ke*ue);
                 NC[elements[i][j]]++;
@@ -158,8 +128,24 @@ int main() {
             TDN[i] /= (double)NC[i];
         }
 
+        double vol = std::accumulate(str.begin(), str.end(), 0.0)/(double)elements.size();
         double ex = Vmax + (volInit - Vmax)*std::max(0.0, 1.0 - (t + 1)/(double)nvol);
         double lambda = std::accumulate(TDN.begin(), TDN.end(), Vector<double>(1))(0)/(double)x.size()*exp(p*((vol - ex)/ex + d));
+
+
+        //----------収束条件の判定----------
+        if(t > nvol && fabs(vol - Vmax) < 0.005 && 
+            fabs(objective[t] - objective[t - 5]) < 0.01*fabs(objective[t]) && 
+            fabs(objective[t] - objective[t - 4]) < 0.01*fabs(objective[t]) && 
+            fabs(objective[t] - objective[t - 3]) < 0.01*fabs(objective[t]) &&
+            fabs(objective[t] - objective[t - 2]) < 0.01*fabs(objective[t]) &&
+            fabs(objective[t] - objective[t - 1]) < 0.01*fabs(objective[t])) {
+            
+            std::cout << "----------Convergence----------" << std::endl;
+            break;
+        }
+
+        std::cout << "t = " << t << "\tCompliance = " << objective[t]/(double)elements.size() << "\tVolume = " << vol << "\tLambda = " << lambda << std::endl;
 
         
         //----------レベルセット関数の更新----------
@@ -172,8 +158,6 @@ int main() {
         for(int i = 0; i < x.size(); i++) {
             TDN[i](0) = C*(TDN[i](0) - lambda);
         }
-
-        std::cout << "t = " << t << "\tCompliance = " << objective[t]/(double)elements.size() << "\tVolume = " << vol << "\tLambda = " << lambda << "\t" << C << std::endl;
 
         std::vector<std::vector<int> > nodetoglobal2 = std::vector<std::vector<int> >(x.size(), std::vector<int>(1, 0));
         SetDirichlet(phi, nodetoglobal2, phifixed);

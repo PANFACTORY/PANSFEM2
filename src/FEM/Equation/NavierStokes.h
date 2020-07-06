e8263bf1cb5d60fdcc509199b9d775c831b4a91f
PANFACTORY <yutatanabe510@gmail.com>
2020-07-06 11:27:37 +0900

    Update NavierStokes.h

--- src/FEM/Equation/NavierStokes.h
+++ src/FEM/Equation/NavierStokes.h
@@ -486,6 +486,49 @@ namespace PANSFEM2 {
 	}
 
 
+	//******************************Get element body force vector******************************
+	template<class T, template<class>class SFU, template<class>class SFP, template<class>class IC, class F>
+	void NavierStokesBodyForce(Vector<T>& _Fe, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementu, const std::vector<int>& _elementu, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementp, const std::vector<int>& _elementp, const std::vector<int>& _doulist, std::vector<Vector<T> >& _x, F _f) {
+        assert(_doulist.size() == 3);
+
+		int m = _elementu.size();   //  Number of shapefunction for velosity u
+        int n = _elementp.size();   //  Number of shapefunction for pressure p
+
+		_Fe = Vector<T>(2*m + n);
+		_nodetoelementu = std::vector<std::vector<std::pair<int, int> > >(m, std::vector<std::pair<int, int> >(2));
+		for(int i = 0; i < m; i++) {
+			_nodetoelementu[i][0] = std::make_pair(_doulist[0], i);
+			_nodetoelementu[i][1] = std::make_pair(_doulist[1], m + i);
+		}
+        _nodetoelementp = std::vector<std::vector<std::pair<int, int> > >(n, std::vector<std::pair<int, int> >(1));
+		for(int i = 0; i < n; i++) {
+			_nodetoelementp[i][0] = std::make_pair(_doulist[2], 2*m + i);
+		}
+
+        Matrix<T> Xp = Matrix<T>(n, 2);
+		for(int i = 0; i < n; i++){
+			Xp(i, 0) = _x[_elementp[i]](0); Xp(i, 1) = _x[_elementp[i]](1);
+		}
+
+		for (int g = 0; g < IC<T>::N; g++) {
+			Vector<T> N = SFP<T>::N(IC<T>::Points[g]);
+			Matrix<T> dNdr = SFP<T>::dNdr(IC<T>::Points[g]);
+			Matrix<T> dXdr = dNdr*Xp;
+			T J = dXdr.Determinant();
+			Vector<T> M = SFU<T>::N(IC<T>::Points[g]);
+			Vector<T> x = Xp.Transpose()*N;
+			Vector<T> b = _f(x);
+
+            Vector<T> Fe = Vector<T>(2*m + n);
+            for(int i = 0; i < m; i++) {
+                Fe(i) = b(0)*M(i);	Fe(i + m) = b(1)*M(i);
+            }
+
+			_Fe += Fe*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
+		}
+	}
+
+
 	//*************************************************************************
 	//	Decoupled method
 	//*************************************************************************
@@ -781,6 +824,48 @@ namespace PANSFEM2 {
 					K(i + m, j + 2*m) = -dMdX(1, i)*N(j);
                     K(j + 2*m, i) = N(j)*dMdX(0, i);                                         						
 					K(j + 2*m, i + m) = N(j)*dMdX(1, i);
+                }
+            }
+			_Ke += K*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
+		}
+	}
+
+
+	//******************************Get element body force matrix with Brinkman model******************************
+	template<class T, template<class>class SFU, template<class>class SFP, template<class>class IC>
+	void NavierStokesBrinkmanBodyForce(Matrix<T>& _Ke, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementu, const std::vector<int>& _elementu, std::vector<std::vector<std::pair<int, int> > >& _nodetoelementp, const std::vector<int>& _elementp, const std::vector<int>& _doulist, std::vector<Vector<T> >& _x, Matrix<T> _k) {
+        assert(_doulist.size() == 3);
+
+		int m = _elementu.size();   //  Number of shapefunction for velosity u
+        int n = _elementp.size();   //  Number of shapefunction for pressure p
+
+		_Ke = Matrix<T>(2*m + n, 2*m + n);
+		_nodetoelementu = std::vector<std::vector<std::pair<int, int> > >(m, std::vector<std::pair<int, int> >(2));
+		for(int i = 0; i < m; i++) {
+			_nodetoelementu[i][0] = std::make_pair(_doulist[0], i);
+			_nodetoelementu[i][1] = std::make_pair(_doulist[1], m + i);
+		}
+        _nodetoelementp = std::vector<std::vector<std::pair<int, int> > >(n, std::vector<std::pair<int, int> >(1));
+		for(int i = 0; i < n; i++) {
+			_nodetoelementp[i][0] = std::make_pair(_doulist[2], 2*m + i);
+		}
+
+        Matrix<T> Xp = Matrix<T>(n, 2);
+		for(int i = 0; i < n; i++){
+			Xp(i, 0) = _x[_elementp[i]](0); Xp(i, 1) = _x[_elementp[i]](1);
+		}
+
+		for (int g = 0; g < IC<T>::N; g++) {
+			Matrix<T> dNdr = SFP<T>::dNdr(IC<T>::Points[g]);
+			Matrix<T> dXdr = dNdr*Xp;
+			T J = dXdr.Determinant();
+			Vector<T> M = SFU<T>::N(IC<T>::Points[g]);
+
+            Matrix<T> K = Matrix<T>(2*m + n, 2*m + n);
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < m; j++) {
+                    K(i, j) = M(i)*_k(0, 0)*M(j);		K(i, j + m) = M(i)*_k(0, 1)*M(j);
+                    K(i + m, j) = M(i)*_k(1, 0)*M(j);   K(i + m, j + m) = M(i)*_k(1, 1)*M(j);  
                 }
             }
 			_Ke += K*J*IC<T>::Weights[g][0]*IC<T>::Weights[g][1];
